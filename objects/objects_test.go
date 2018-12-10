9c3a8d496ffac4a6503e9206e0bb01db50851eab
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-12-10 10:05:39 +0300

    Problem: coverage is not calculated correctly.
    Solution: add dummy tests to packages w/o tests.

---
+++ objects/objects_test.go
@@ -0,0 +1,17 @@
+// Gosh programming language.
+// Copyright (c) 2018 Alexey Palazhchenko and contributors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package objects
+
+import (
+	"testing"
+)
+
+// This dummy test exists for correct test coverage calculations.
+// Remove it when real tests are added.
+func TestDummy(t *testing.T) {
+}
